c008d2c4e2b75b6460517fa119c38cd28e4bd255
csatsurvey <haliunaa.dolgorjav@gmail.com>
2026-06-21 08:09:59 +0800

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,1 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getDatabase } from 'firebase/database';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCaMrXWbpZ2PWAz0n7Pg3BSfQlbWKmUIp0",
+  authDomain: "restaurant-system-6fb57.firebaseapp.com",
+  databaseURL: "https://restaurant-system-6fb57-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "restaurant-system-6fb57",
+  storageBucket: "restaurant-system-6fb57.firebasestorage.app",
+  messagingSenderId: "423284471398",
+  appId: "1:423284471398:web:1040e86fa3fe373e7c5fa7"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getDatabase(app);
 
